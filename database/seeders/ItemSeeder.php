1513ef001940bb6d88c8ff1ef5e2b1fd620516e4
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-06-17 16:17:58 +0530

    create Database seeders

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
 
 class ItemSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+     public function run(): void
     {
-        //
+        Item::create([
+            'name' => 'Espresso',
+            'price' => 250.00,
+            'description' => 'Strong black coffee'
+        ]);
+
+        Item::create([
+            'name' => 'Cappuccino',
+            'price' => 300.00,
+            'description' => 'Coffee with milk foam'
+        ]);
+
+        Item::create([
+            'name' => 'Mocha',
+            'price' => 350.00,
+            'description' => 'Chocolate flavored coffee'
+        ]);
     }
 }
